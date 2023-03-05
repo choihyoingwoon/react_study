39d65d843ae75c1ff57affc89e5eb01b9429c8a5
choihyoingwoon <cpobo@hanmail.net>
2023-03-05 21:43:35 +0900

    03/05 section 7

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Accommodate from './chapter_07/Accommodate';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,8 @@ import reportWebVitals from './reportWebVitals';
 // import Library from './chapter_03/Library';
 // import Clock from './chapter_04/Clock';
 // import CommentList  from './chapter_05/CommentList';
-import NotificationList from './chapter_06/NotificationList';
+//import NotificationList from './chapter_06/NotificationList';
+
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(/
 //   <React.StrictMode>
@@ -16,7 +18,7 @@ import NotificationList from './chapter_06/NotificationList';
 // );
 ReactDOM.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accommodate />
   </React.StrictMode>,
   document.getElementById('root')
 );
